6915e39410fdfa5bab442b6a3df54d9c26c4001d
jeffeDurand <jfdurand.droid@gmail.com>
2025-04-04 07:22:59 -0400

    setup with gcs

--- dataset_toolkits/setup.sh
+++ dataset_toolkits/setup.sh
@@ -1,1 +1,1 @@
-pip install pillow imageio imageio-ffmpeg tqdm easydict opencv-python-headless pandas open3d objaverse huggingface_hub bpy==4.1 fake-bpy-module-4.1
+pip install pillow imageio imageio-ffmpeg tqdm easydict opencv-python-headless pandas open3d objaverse huggingface_hub bpy==4.1 fake-bpy-module-4.1 google-cloud-storage
